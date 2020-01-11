5bbdbf0f038eb0aeabfb232acbbbbcd328adc802
Daniil Krasnov <dk98126@yandex.ru>
2020-01-11 17:28:49 +0300

    Initial commit

---
+++ client/src/dk98126/chat/client/ClientWindow.java
@@ -0,0 +1,94 @@
+package dk98126.chat.client;
+
+import dk98126.chat.network.TCPConnection;
+import dk98126.chat.network.TCPConnectionListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
+
+    private static final String IP_ADDR = "192.168.1.71";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickName = new JTextField("alex");
+    private final JTextField fieldInput = new JTextField();
+
+    private TCPConnection connection;
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickName, BorderLayout.NORTH);
+
+        setVisible(true);
+        try {
+            connection = new TCPConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMessage("Connection exception: " + e);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = fieldInput.getText();
+        if (msg.isEmpty()) return;
+        fieldInput.setText(null);
+        connection.sendString(fieldNickName.getText() + ": " + msg);
+    }
+
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMessage("Connection ready...");
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+        printMessage(value);
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMessage("Connection close");
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMessage("Connection exception: " + e);
+    }
+
+    private synchronized void printMessage(String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
+    }
+}
---
+++ network/src/dk98126/chat/network/TCPConnectionListener.java
@@ -0,0 +1,10 @@
+package dk98126.chat.network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onException(TCPConnection tcpConnection, Exception e);
+
+}
